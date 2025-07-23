f351170aad38606de18e76e09dc19a94a2a8c1f0
Sharawelly <lordmarawanshrief@gmail.com>
2025-07-23 12:23:50 +0300

    fix get user data

--- lib/views/profile/ui/profile_view.dart
+++ lib/views/profile/ui/profile_view.dart
@@ -15,74 +15,78 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<AuthenticationCubit, AuthenticationState>(
-      listener: (context, state) {
-        if (state is LogoutSuccess) {
-          navigateWithoutBack(context, LoginView());
-        }
-      },
-      builder: (context, state) {
-        AuthenticationCubit cubit = context.read<AuthenticationCubit>();
-        return state is LogoutLoading
-            ? CustomCircleProgIndicator()
-            : Center(
-                child: SizedBox(
-                  height: MediaQuery.sizeOf(context).height * .65,
-                  child: Card(
-                    color: AppColors.kWhiteColor,
-                    margin: const EdgeInsets.all(24),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(16)),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        children: [
-                          const CircleAvatar(
-                            radius: 55,
-                            backgroundColor: AppColors.kPrimaryColor,
-                            foregroundColor: AppColors.kWhiteColor,
-                            child: Icon(Icons.person, size: 45),
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            cubit.userDataModel?.name.toString() ?? "User Name",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            cubit.userDataModel?.email.toString() ??
-                                "User Email",
-                          ),
-                          const SizedBox(height: 10),
-                          CustomRowBtn(
-                            onTap: () =>
-                                navigateTo(context, const EditNameView()),
-                            icon: Icons.person,
-                            text: "Edit Name",
-                          ),
-                          const SizedBox(height: 10),
-                          CustomRowBtn(
-                            onTap: () =>
-                                navigateTo(context, const MyOrdersView()),
-                            icon: Icons.shopping_basket,
-                            text: "My Orders",
-                          ),
-                          const SizedBox(height: 10),
-                          CustomRowBtn(
-                            onTap: () async {
-                              await cubit.signOut();
-                            },
-                            icon: Icons.logout,
-                            text: "Logout",
-                          ),
-                        ],
+    return BlocProvider(
+      create: (context) => AuthenticationCubit()..getUserData(),
+      child: BlocConsumer<AuthenticationCubit, AuthenticationState>(
+        listener: (context, state) {
+          if (state is LogoutSuccess) {
+            navigateWithoutBack(context, LoginView());
+          }
+        },
+        builder: (context, state) {
+          AuthenticationCubit cubit = context.read<AuthenticationCubit>();
+          return state is LogoutLoading || state is GetUserDataLoading
+              ? CustomCircleProgIndicator()
+              : Center(
+                  child: SizedBox(
+                    height: MediaQuery.sizeOf(context).height * .65,
+                    child: Card(
+                      color: AppColors.kWhiteColor,
+                      margin: const EdgeInsets.all(24),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(16)),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            const CircleAvatar(
+                              radius: 55,
+                              backgroundColor: AppColors.kPrimaryColor,
+                              foregroundColor: AppColors.kWhiteColor,
+                              child: Icon(Icons.person, size: 45),
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              cubit.userDataModel?.name.toString() ??
+                                  "User Name",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              cubit.userDataModel?.email.toString() ??
+                                  "User Email",
+                            ),
+                            const SizedBox(height: 10),
+                            CustomRowBtn(
+                              onTap: () =>
+                                  navigateTo(context, const EditNameView()),
+                              icon: Icons.person,
+                              text: "Edit Name",
+                            ),
+                            const SizedBox(height: 10),
+                            CustomRowBtn(
+                              onTap: () =>
+                                  navigateTo(context, const MyOrdersView()),
+                              icon: Icons.shopping_basket,
+                              text: "My Orders",
+                            ),
+                            const SizedBox(height: 10),
+                            CustomRowBtn(
+                              onTap: () async {
+                                await cubit.signOut();
+                              },
+                              icon: Icons.logout,
+                              text: "Logout",
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              );
-      },
+                );
+        },
+      ),
     );
   }
 }
